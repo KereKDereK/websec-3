14d5124c003a22b8417958d44e5c9d60ba585ddd
KereK DereK <kerekderek1@gmail.com>
2022-12-19 16:39:14 +0400

    orm configured

---
+++ Server/Server/Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Server.Models
+
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!;
+        public DbSet<Image> Images { get; set; } = null!;
+        public DbSet<Like> Likes { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<Subscriptions> Subscriptions { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user=student;password=P@ssw0rd;database=stogramm;",
+                ServerVersion.AutoDetect("server=localhost;user=student;password=P@ssw0rd;database=stogramm;"));
+        }
+    }
+}
--- Server/Server/Models/User.cs
+++ Server/Server/Models/User.cs
@@ -6,8 +6,8 @@
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
-        public int RegistrationType { get; set; }
-        public User(int userId, string userName,string email, string passwordHash, int registrationType)
+        public string RegistrationType { get; set; }
+        public User(int userId, string userName,string email, string passwordHash, string registrationType)
         {
             UserId = userId;
             UserName = userName;
