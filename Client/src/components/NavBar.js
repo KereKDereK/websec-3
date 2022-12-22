c73cb4d493a13f39170671db68fadb0b7aec0e0a
LONHik4 <gaynullin.dmitriy@gmail.com>
2022-12-22 21:13:40 +0400

    commit

--- Client/src/components/NavBar.js
+++ Client/src/components/NavBar.js
@@ -5,26 +5,30 @@ import Container from "react-bootstrap/Container";
 import { useContext } from 'react'
 import { Context } from '../index';
 import { HOME_ROUTE,AUTH_ROUTE, REG_ROUTE } from "../utils/consts";
-import User from "../pages/User";
 import  Button  from "react-bootstrap/Button";
+import {observer} from "mobx-react-lite";
 
-export function NavBar() {
+const NavBar = observer( () => {
     const {user} = useContext(Context)
     return (
       <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href={HOME_ROUTE}>Navbar</Navbar.Brand>
-          {user.isAuth ?
-            <Nav className="ml-auto">
-              <Button variant={"outline-light"}>XYY</Button>
-            </Nav>
-            :
-            <Nav className="ml-auto">
-              <Button variant={"outline-light"}>Log in</Button>
-              <Button variant={"outline-light"}>Register</Button>
-            </Nav>
-          }
+        <Container>          
+          <Navbar.Brand href={HOME_ROUTE}>StoGramm</Navbar.Brand>
+            {user.isAuth?
+              <Nav className="ml-auto" style={{color : 'white'}}>
+                <Button variant={"outline-light"}>XYY</Button>
+              </Nav>
+              :
+              <Nav className="ml-auto">
+                <a href="https://oauth.yandex.ru/authorize?response_type=code&client_id=9ad6adf4562c48399e6da3cc61272b92">
+                  <Button variant={"outline-light"}style={{color : 'white'}} onClick={() => user.setIsAuth(true)}>
+                    Войти с помощью яндекса
+                  </Button>
+                </a> 
+              </Nav>
+            }</Container>
       </Navbar>
     )
-}
+})
 
 export default NavBar;
