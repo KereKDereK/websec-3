fc2e0fa2cdcd9d5ee168066aef671de6ce7aed2f
KereK DereK <kerekderek1@gmail.com>
2022-12-23 03:18:11 +0400

    what

--- Client/src/index.js
+++ Client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import UserAcc from "./Stogramm/UserAcc";
+import NavBar from './components/NavBar'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 export const Context = createContext(null)
@@ -16,6 +17,7 @@ root.render(
     user: new UserAcc()
   }}>
     <BrowserRouter>
+    <NavBar/>
     <App />
     </BrowserRouter>
   </Context.Provider>
