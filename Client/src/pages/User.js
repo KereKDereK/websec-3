da23b80de5275901b4cbce59ae3eeb784635cf55
KereK DereK <kerekderek1@gmail.com>
2022-12-24 02:07:12 +0400

    successful data retrieve from server

--- Client/src/pages/User.js
+++ Client/src/pages/User.js
@@ -3,21 +3,32 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import axios from "axios"
+import {useState, useEffect} from "react"
 
 function User() {
     const id = window.location.href.toString().split("/")[4]
-    console.log(id)
     axios.defaults.baseURL = 'https://localhost:5001';
-    let name =axios.get('/api/User/'+id,{ withCredentials: true })
-    console.log(name.then((data)=>data))
+    const [state, setState] = useState();
+    useEffect (() => {
+        async function retrieveData() 
+        {
+            const response = await axios.get('/api/User/'+id,{ withCredentials: true })
+            .then((response) => setState(response))
+            return response
+        }
+        retrieveData()
+    }, [])
+
+    console.log(state)
     return (
         <Form>
         <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
             <Form.Label column sm="2">
-                {name}
+            Пышки и мышьки
             </Form.Label>
             <Col sm="10">
-            <Form.Control plaintext readOnly defaultValue="ИВАН Иванов" />
+            {state==undefined ? " " : state.data.userName}
+            <Form.Control plaintext readOnly />
             </Col>
         </Form.Group>
     </Form>
